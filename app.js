044c74694b65808cffd32c6d0cda8aeb889bda00
Adam <Adamwage@gmail.com>
2019-01-18 22:56:57 -0600

    Added a MONGO_DB env var to name the dev database my data will be pushed to.

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use('/graphql', graphqlHttp({
 
 // Mongoose Connection
 mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-q1v7l.mongodb.net/test?retryWrites=true`,
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-q1v7l.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
     {useNewUrlParser: true}
 )
 .then(() => {
