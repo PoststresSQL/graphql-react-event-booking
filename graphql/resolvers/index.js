1949f5094524c8fc9579bc215ed23e46d9f4ca6a
Adam Age <Adamwage@hotmail.com>
2019-01-25 05:25:25 -0600

    bookings query for GraphQL api created to fetch all bookings. Not specific to user, yet.

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 // Models
 const Event = require('../../models/event')
 const User = require('../../models/user')
+const Booking = require('../../models/booking')
 
 // function to find multiple event IDs
 const events = (eventIds) => {
@@ -41,19 +42,19 @@ const user = (userId) => {
 module.exports = {
     events: () => {
         return Event.find()
-            .then((events) => {
-                return events.map(event => {
-                    return {
-                        ...event._doc,
-                        _id: event.id,
-                        date: new Date(event._doc.date).toISOString(),
-                        creator: user.bind(this, event._doc.creator)
-                    }
-                })
+        .then((events) => {
+            return events.map(event => {
+                return {
+                    ...event._doc,
+                    _id: event.id,
+                    date: new Date(event._doc.date).toISOString(),
+                    creator: user.bind(this, event._doc.creator)
+                }
             })
-            .catch(err => {
-                throw err
-            })
+        })
+        .catch(err => {
+            throw err
+        })
     },
     createEvent: (args) => {
         const event = new Event({
@@ -67,33 +68,49 @@ module.exports = {
         // save method brought in by mongoose that will
         // push to the connected db
         return event
-            .save()
-            .then(result => {
-                createdEvent = {
-                    ...result._doc,
-                    _id: event._doc._id.toString(),
-                    date: new Date(event._doc.date).toISOString(),
-                    creator: user.bind(this, result._doc.creator)
+        .save()
+        .then((result) => {
+            createdEvent = {
+                ...result._doc,
+                _id: event._doc._id.toString(),
+                date: new Date(event._doc.date).toISOString(),
+                creator: user.bind(this, result._doc.creator)
+            }
+            return User.findById('5c43f44aacb554920d1bd26a')
+        })
+        .then((user) => {
+            if (!user) {
+                throw new Error('User not found.')
+            }
+            user.createdEvents.push(event)
+            return user.save()
+        })
+        .catch((err) => {
+            throw err
+        })
+    },
+    bookings: async () => {
+        try {
+            // fetch bookings from DB
+            const bookings = await Booking.find()
+            return bookings.map((booking) => {
+                return {
+                    ...booking._doc,
+                    _id: booking.id,
+                    createdAt: new Date(booking._doc.createdAt).toISOString,
+                    updatedAt: new Date(booking._doc.createdAt).toISOString
                 }
-                return User.findById('5c43f44aacb554920d1bd26a')
             })
-            .then((user) => {
-                if (!user) {
-                    throw new Error('User not found.')
-                }
-                user.createdEvents.push(event)
-                return user.save()
-            })
-            .catch(err => {
-                throw err
-            })
+        } catch (err) {
+            throw err
+        }
     },
     createUser: (args) => {
         // Check DB for a user with same email as one being created
         return User.findOne({
             email: args.userArgs.email
         })
-            .then(user => {
+            .then((user) => {
                 // If User already has the email, throw an error
                 if (user) {
                     throw new Error('User Already Exists')
@@ -107,18 +124,18 @@ module.exports = {
                         })
                         return user.save()
                     })
-                    .then(result => {
+                    .then((result) => {
                         return {
                             ...result._doc,
                             password: null,
                             _id: result._doc._id.toString()
                         }
                     })
-                    .catch(err => {
+                    .catch((err) => {
                         throw err
                     })
             })
-            .catch(err => {
+            .catch((err) => {
                 throw err
             })
     }
